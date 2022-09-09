7e781ab405c46056b2425612aefafffc3753b5a9
zad65536 <92348477+zad65536@users.noreply.github.com>
2022-09-10 01:48:31 +0800

    Add files via upload

---
+++ lisbon-puzzle-sponsor.js
@@ -0,0 +1,212 @@
+const atoPuzzleSponsor = new Object();
+var id;
+
+id="octopus";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Octopus Network";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/octopus-network-oct-logo.png";
+atoPuzzleSponsor[id]["web"]="https://oct.network/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/oct_network";
+
+id="debio";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Debio Network";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/08/debiologo.png";
+atoPuzzleSponsor[id]["web"]="https://www.debio.network/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/debio_network";
+
+id="myriad";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Myriad Social";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/08/jHXi6y5_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://www.myriad.social/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/myriad_social";
+
+id="deip";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="DEIP";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/08/XRfB1PCu_400x400.png";
+atoPuzzleSponsor[id]["web"]="https://deip.world/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/deip_world";
+
+id="unique";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Unique One Network";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/08/uone-logo-128.png";
+atoPuzzleSponsor[id]["web"]="https://www.unique.one/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/UniqueOne_UO";
+
+id="qstn";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="QSTN";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/08/qstn2.jpg";
+atoPuzzleSponsor[id]["web"]="https://qstn.us";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/qstnus";
+
+id="insider";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Near Insider";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/AKWpPdzy_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://twitter.com/near_insider";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/near_insider";
+
+id="master";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Near Master";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/h_RmYyF4_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://linktr.ee/Master_Ecosystem";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/Master_Near";
+
+id="nearity";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Nearity";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/r3J4sxCZ_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="http://nearity.org/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/Nearity_org";
+
+id="games";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Near Games Guild";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/0dPdEfmE_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://twitter.com/NearGamesGuild";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/NearGamesGuild";
+
+id="nigeria";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Near Nigeria Guild";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/pydGOuZj_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://nearnigeria.org/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/NearprotocolNG";
+
+id="vietnam";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Near Vietnam Guild";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/uiNy0Ekh_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://medium.com/@nearauroravietnam";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/VietnamNear";
+
+id="malaysia";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Near Malaysia Guild";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/tWuyDCv8_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://www.mynear.org/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/near_malaysia";
+
+id="singapore";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Near Singapore Guild";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/b5fPPSzk_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://seedweb3.io/near-sg";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/NEAR_Singapore";
+
+id="france";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Near France Guild";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/esMbBIGH_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://twitter.com/FrancaisNear";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/FrancaisNear";
+
+id="paras";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Paras NFT Marketplace";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/Rhlq_p3__400x400.png";
+atoPuzzleSponsor[id]["web"]="https://paras.id/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/ParasHQ";
+
+id="3six9";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="3six9 NFT Marketplace";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/08/wnP7pqtJ_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://3six9.space/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/3six9OFFICIAL";
+
+id="ref";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Ref Finance";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/rvJtgIsU_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://www.ref.finance/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/finance_ref";
+
+id="skyward";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Skyward Finance";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/xNPSXV5J_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://skyward.finance/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/skywardfinance";
+
+id="starter";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="NEARStarter";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/fVs8JpcB_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://nearstarter.fi/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/NEARStarter_fi";
+
+id="stars";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Near Stars Guild";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/RJ0LgiRy_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://gov.near.org/t/near-stars-guild/1387";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/NearStarsGuild";
+
+id="aurora";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Aurora";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/TDDL2hxU_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://aurora.dev/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/auroraisnear";
+
+id="foundation";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Near Foundation (NEARCON)";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/NEAR-LOGO.png";
+atoPuzzleSponsor[id]["web"]="https://nearcon.org/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/nearprotocol";
+
+id="vision";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Vision Chain";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/08/W0jkQVDm_400x400.png";
+atoPuzzleSponsor[id]["web"]="https://v.network/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/Vision_Chain";
+
+id="skyharbour";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Skyharbour (The Rust Guild)";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/Tv3rtslK_400x400-1.jpg";
+atoPuzzleSponsor[id]["web"]="http://skyharbour.nft";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/SkyHarbourNFT";
+
+id="tenk";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="TenkDAO";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/08/profile-img.jpg";
+atoPuzzleSponsor[id]["web"]="https://tenkbay.com/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/TenKBay";
+
+id="brown";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Mr. Brown NFT";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/WgOX5tCA_400x400.png";
+atoPuzzleSponsor[id]["web"]="https://mrbrownproject.com/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/MrBrownNFT";
+
+id="misfits";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Near Misfits";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/DggEwqT_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://nearmisfits.com/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/nearmisfits";
+
+id="together";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Together Coin";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/um0kcqVO_400x400.jpg";
+atoPuzzleSponsor[id]["web"]="https://www.togetherwerule.com/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/tgtwtf";
+
+id="roketo";
+atoPuzzleSponsor[id]=new Object();
+atoPuzzleSponsor[id]["title"]="Roketo";
+atoPuzzleSponsor[id]["logo"]="https://atocha.io/wp-content/uploads/2022/09/2Ws14PAi_400x400.png";
+atoPuzzleSponsor[id]["web"]="https://roke.to/";
+atoPuzzleSponsor[id]["twitter"]="https://twitter.com/roketostream";
