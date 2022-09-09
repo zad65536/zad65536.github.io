bb34adf64a4757c74feeb8e76997ff18b26cd9d4
zad65536 <92348477+zad65536@users.noreply.github.com>
2022-09-10 01:47:48 +0800

    Add files via upload

---
+++ lisbon-puzzle-admin.js
@@ -0,0 +1,123 @@
+const atoPuzzleAdmin = new Object();
+var id;
+
+id="octopus";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="U2FsdGVkX19Zqogh8GxP8/FY4YbbwjZBwX/1gQT29cs=";
+
+id="debio";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="U2FsdGVkX1+Y21xNEZPWUJhWkGdHC9POsPtGoX/jV48=";
+
+id="myriad";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="deip";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="U2FsdGVkX1+7tEiLt34v6eJLXlKexa5Bdy9TfXr0yzw=";
+
+id="unique";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="U2FsdGVkX18KDlLlHIhKDPHy7lvyuJaY/xlFYLmpS43OEmOJS5aUN9siRe+dfiD6j7Qjhi4B0PhsXqnPYvVh0w==";
+
+id="qstn";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="U2FsdGVkX1+xTpf0TE+YuiWTkeOrEtxUuzcLQSX51Jw=";
+
+id="insider";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="master";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="U2FsdGVkX1+0jfhrv54Ieu14I8yIOOGPr1qNmL4KcLQmn7nd+vGgiEy9n72It3ON";
+
+id="nearity";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="games";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="nigeria";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="vietnam";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="malaysia";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="singapore";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="france";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="paras";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="3six9";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="ref";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="skyward";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="starter";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="stars";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="aurora";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="U2FsdGVkX18MXdcguzYuRZXPQ+a7NaAz8f68KwjHM5g=";
+
+id="foundation";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="vision";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="U2FsdGVkX193qM1xqQ5Qd1wgg5XQhXg18UvLbbZ9lHQ=";
+
+id="skyharbour";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="tenk";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="brown";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
+id="misfits";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="U2FsdGVkX1/CStJ2wBWNAH15BYR08e5CNm7cbbDpko0=";
+
+id="together";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="U2FsdGVkX18wKhwuoRuP4HsVzHwgZGd2Gq8iivus5xo=";
+
+id="roketo";
+atoPuzzleAdmin[id] = new Object();
+atoPuzzleAdmin[id]["answerAesEncrypt"]="xxx";
+
