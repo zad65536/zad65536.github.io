f9629232385795ce13a1e369a72cf66443e5ed04
zad65536 <92348477+zad65536@users.noreply.github.com>
2022-09-09 14:32:25 +0800

    Create lisbon-puzzle-status.js

---
+++ lisbon-puzzle-status.js
@@ -0,0 +1,16 @@
+const atoPuzzleStatus = new Object();
+
+atoPuzzleStatus["octopus"] = new Object();
+atoPuzzleStatus["octopus"]["confirmedWinners"]=0;
+atoPuzzleStatus["octopus"]["ifOpenForClaiming"]="yes";
+atoPuzzleStatus["octopus"]["lastUpdateTime"]="202209090930";
+
+atoPuzzleStatus["deip"] = new Object();
+atoPuzzleStatus["deip"]["confirmedWinners"]=0;
+atoPuzzleStatus["deip"]["ifOpenForClaiming"]="yes";
+atoPuzzleStatus["deip"]["lastUpdateTime"]="202209090930";
+
+atoPuzzleStatus["misfits"] = new Object();
+atoPuzzleStatus["misfits"]["confirmedWinners"]=0;
+atoPuzzleStatus["misfits"]["ifOpenForClaiming"]="yes";
+atoPuzzleStatus["misfits"]["lastUpdateTime"]="202209090930";
