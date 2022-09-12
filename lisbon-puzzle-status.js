5b8a4d116e61a1445969d98683da3f4d1ffccd2d
AtoK <90557872+linhai0212@users.noreply.github.com>
2022-09-12 13:55:00 +0800

    Update lisbon-puzzle-status.js

--- lisbon-puzzle-status.js
+++ lisbon-puzzle-status.js
@@ -9,8 +9,8 @@ atoPuzzleStatus[id]["lastUpdateTime"]="2022-0911-1053-GMT";
 
 id="debio";
 atoPuzzleStatus[id] = new Object();
-atoPuzzleStatus[id]["confirmedWinners"]=7;
-atoPuzzleStatus[id]["ifOpenForClaiming"]="yes"; 
+atoPuzzleStatus[id]["confirmedWinners"]=10; // To max
+atoPuzzleStatus[id]["ifOpenForClaiming"]="no"; 
 atoPuzzleStatus[id]["lastUpdateTime"]="2022-0911-1053-GMT";
 
 id="myriad";
