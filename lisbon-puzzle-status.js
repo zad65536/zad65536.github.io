310b6f177f94822a1b3359fb1986831cfeda3a88
zad65536 <92348477+zad65536@users.noreply.github.com>
2022-09-13 07:42:53 +0800

    Update lisbon-puzzle-status.js

--- lisbon-puzzle-status.js
+++ lisbon-puzzle-status.js
@@ -153,8 +153,8 @@ atoPuzzleStatus[id]["lastUpdateTime"]="2022-0909-2359-GMT";
 
 id="tenk";
 atoPuzzleStatus[id] = new Object();
-atoPuzzleStatus[id]["confirmedWinners"]=0;
-atoPuzzleStatus[id]["ifOpenForClaiming"]="yes";
+atoPuzzleStatus[id]["confirmedWinners"]=5;
+atoPuzzleStatus[id]["ifOpenForClaiming"]="no";
 atoPuzzleStatus[id]["lastUpdateTime"]="2022-0909-2359-GMT";
 
 id="brown";
